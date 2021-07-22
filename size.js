9237407eac89b3dab6376c37c9c7a1b1293459ec
tessabrazda <78933290+tessabrazda@users.noreply.github.com>
2021-07-22 13:23:11 -0500

    Update size.js
    see size.html for how to use the program.

--- size.js
+++ size.js
@@ -1,35 +1,28 @@
-//Write a script that prints out the filesize, width and height of every image
-// in the attached spreadsheet, and displays it in spreadsheet format.
+const url = "https://api.cloudinary.com/v1_1/demo/image/upload";
+const form = document.querySelector("form");
 
-async function getData(){
-  //API call for a single image (functions on example, doesn't work on images from excel sheet)
-  //need to upload files first to call this api on them. have list of image urls stored in links.txt
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  var image = "boat_lake2.jpg"; //example image used on website to demonstrate the API call
-  //other images won't work. Still trying to figure out why and how to make them work.
-  //images need to be uploaded to use the fl_getinfo api call, but uploading isn't working.
-  
-  //What I know of the Cloudinary Upload API call:
-  //all of the following strings entries:
-    //file can be a url
-    //upload_preset must be unsigned
-    //signature requires authentication? automatically done with the SDKs? Requires an account?
-    //unsigned format in Node.JS: cloudinary.v2.uploader.unsigned_upload(file, upload_preset, options, callback);
+  const files = document.querySelector("[type=file]").files;
+  const formData = new FormData();
 
-  var ApiString = "https://res.cloudinary.com/demo/image/upload/fl_getinfo/" + image;
-  var response = await fetch(ApiString);
-  var jsonData = await response.json();
-  var output = jsonData.output //simplifies the code later
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+    formData.append("file", file);
+    formData.append("upload_preset", "docs_upload_example_us_preset");
 
-  //prints data in CSV format --> can easily be made into excel sheets
-  document.getElementById("data").innerHTML += "<br>" + output.bytes + "," + output.width + "," + output.height;
-}
-
-function formatData(){
-  //was going to make this into a recursive function to call this on the list of url strings to output the data
-  //need getData() to work on uploaded images first to finalize this part
-  //for(image in links.txt){
-    getData();
-  //}
-}
-
+    fetch(url, {
+      method: "POST",
+      body: formData
+    })
+      .then((response) => {
+        return response.json();
+        //return response.text(); //--> for testing purposes
+      })
+      .then((data) => {
+        document.getElementById("data").innerHTML += "<br>" + data.secure_url + "," + data.bytes + "," + data.width + "," + data.height;
+        //document.getElementById("data").innerHTML += data //--> for testing puurposes
+      });
+  }
+});
